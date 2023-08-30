0e6e495faa17273ed1850bfa2e5b34945031eb4f
saanspeur <evelin.ortega018@gmail.com>
2023-08-29 20:26:43 -0700

    added css need to do js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// code for search button
 
+document.querySelector("#searchButton").addEventListener("click", () => {
+  let city = document.querySelector("#citySearch").value;
+  let state = document.querySelector("#stateSearch").value;
+  console.log(city, state);
+});
